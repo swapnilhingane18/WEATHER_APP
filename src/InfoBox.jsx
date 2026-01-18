d268cb84f6cca7f7a4e09e0e52a3dc68e8895af1
swapnilhingane18 <swapnilhingane9290@gmail.com>
2026-01-18 23:59:42 +0530

    extra removed

--- src/InfoBox.jsx
+++ src/InfoBox.jsx
@@ -18,13 +18,14 @@ if (weatherType === "Clouds") {
   imageURL = "https://images.unsplash.com/photo-1499346030926-9a72daac6c63?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGNsb3VkfGVufDB8fDB8fHww";
 } else if (weatherType === "Clear") {
   imageURL = "https://images.unsplash.com/photo-1590077428593-a55bb07c4665?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGNsZWFyJTIwc2t5fGVufDB8fDB8fHww";
-} else if (weatherType === "Rain" || weatherType === "Drizzle") {
-  imageURL = "https://media.istockphoto.com/id/498063665/photo/rainy-landscape.webp?a=1&b=1&s=612x612&w=0&k=20&c=hOE6L7f7OoSKUW1Q4tR27GoEkOU_ywKJGCvSO77SeZg=";
-} else if (weatherType === "Snow") {
-  imageURL = "https://plus.unsplash.com/premium_photo-1670106817708-a914a1634370?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c25vdyUyMHNreXxlbnwwfHwwfHx8MA%3D%3D";
-} else if (weatherType === "Thunderstorm") {
-  imageURL = "https://images.unsplash.com/photo-1507607422988-9a1b54b0c3d8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHRodW5kZXJ3YW5kJTIwc2t5fGVufDB8fDB8fHww";
-}
+
+git   // } else if (weatherType === "Rain" || weatherType === "Drizzle") {
+//   imageURL = "https://media.istockphoto.com/id/498063665/photo/rainy-landscape.webp?a=1&b=1&s=612x612&w=0&k=20&c=hOE6L7f7OoSKUW1Q4tR27GoEkOU_ywKJGCvSO77SeZg=";
+// } else if (weatherType === "Snow") {
+//   imageURL = "https://plus.unsplash.com/premium_photo-1670106817708-a914a1634370?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c25vdyUyMHNreXxlbnwwfHwwfHx8MA%3D%3D";
+// } else if (weatherType === "Thunderstorm") {
+//   imageURL = "https://images.unsplash.com/photo-1507607422988-9a1b54b0c3d8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHRodW5kZXJ3YW5kJTIwc2t5fGVufDB8fDB8fHww";
+// }
   
 
   if (!weatherInfo) return null;
